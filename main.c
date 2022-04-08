a68dc26dee10b5603e1b026aea35a97c44b1f9ed
Borjados <borja.diazdeotazu@opendeusto.es>
2022-04-08 11:48:46 +0200

    Update Usuario y Admin
    Añadida parte de usuario y administrador
    Las cosas que están comentadas es donde deberian ir las consultas a Base de Datos

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 int main(){
 
 	setvbuf(stdout, NULL, _IONBF, 0);
-	int seleccion, DNI, tarjeta, telefono;
+	int seleccion, DNI, tarjeta, telefono, selecadmin, bono;
 	char nombre[20];
 	char username[20];
 	char apellidos[30];
@@ -23,7 +23,7 @@ int main(){
 	printf("2.Registrarse\n");
 	printf("3.Cerrar\n");
 
-	scanf("%i", &seleccion);
+	scanf(" %i", &seleccion);
 
 	while (seleccion != 3){
 
@@ -32,12 +32,84 @@ int main(){
 		printf("\nINICIAR SESION\n");
 		printf("--------------\n");
 		printf("Introduzca su nombre: ");
-		scanf("%s", nombre);
+		scanf(" %s", nombre);
 		printf("Introduzca su contraseña: ");
-		scanf("%s", contrasena);
+		scanf(" %s", contrasena);
 		printf("Nombre:%s, Contraseña:%s", nombre, contrasena);
 		if(i == 1){
 			//Aqui es para la consulta de si el usuario esta registrado, entonces sigue el programa, si no pasa al else
+			//Usuario Normal
+			printf("No disponible :(");
+		}
+		else if(i == 2){
+			//Aqui es para la consulta de si el usuario esta registrado, entonces sigue el programa, si no pasa al else
+			//Administrador
+
+		while (selecadmin != 4){
+
+			printf("¡BIENVENIDO ADMINISTRADOR!\n");
+			printf("------------------------------\n");
+			printf("1.Ver estadisticas\n");
+			printf("2.Modificar Bonos\n");
+			printf("3.Acceder a los ficheros\n");
+			printf("4.Salir\n");
+
+			scanf(" %i", &selecadmin);
+
+			switch (selecadmin) {
+				case 1:
+					printf("Aqui se ven las estadisticas. No disponible\n");
+				break;
+				case 2:
+					// Aqui va el codigo para poder modificar los datos de los bonos
+
+					while(bono != 5){
+					printf("BONOS\n");
+					printf("------------------------------\n");
+					printf("Seleccione el bono que desea modificar\n");
+					printf("\n");
+					printf("1.Diario\n");
+					printf("2.Semanal\n");
+					printf("3.Mensual\n");
+					printf("4.Cuatrimensual\n");
+					printf("5.Salir\n");
+
+					scanf(" %i", &bono);
+
+					switch(bono){
+
+					case 1:
+						//codigo para modificacion del bono diario
+					break;
+					case 2:
+						//codigo para modificacion del bono semanal
+					break;
+					case 3:
+						//codigo para modificacion del bono mensual
+					break;
+					case 4:
+						//codigo para modificacion del bono diario
+					break;
+					case 5:
+						bono = 5;
+					break;
+
+					}
+				}
+
+				break;
+				case 3:
+					printf("Aqui se ven los ficheros. No disponible\n");
+				break;
+				case 4:
+					selecadmin = 4;
+				break;
+				default:
+					break;
+			}
+
+		}
+
 		}
 		else{
 			printf("\nDebes registrarte primero\n");
@@ -49,21 +121,21 @@ int main(){
 		printf("\nRegistro\n");
 		printf("--------------\n");
 		printf("Introduzca su nombre: ");
-		scanf("%s", nombre);
+		scanf(" %s", nombre);
 		printf("Introduzca sus apellidos: ");
-		scanf("%s", apellidos);
+		scanf(" %s", apellidos);
 		printf("Introduzca su DNI: ");
-		scanf("%i", &DNI);
+		scanf(" %i", &DNI);
 		printf("Introduzca su Tarjeta: ");
-		scanf("%i", &tarjeta);
+		scanf(" %i", &tarjeta);
 		printf("Introduzca su Telefono: ");
-		scanf("%i", &telefono);
+		scanf(" %i", &telefono);
 		printf("Introduzca su nombre de usuario: ");
-		scanf("%s", username);
+		scanf(" %s", username);
 		printf("Introduzca su contraseña: ");
-		scanf("%s", contrasena);
+		scanf(" %s", contrasena);
 		printf("Es profersor(p),Estudiante(e) o Regular(r): ");
-		scanf("%s", &tipo);
+		scanf(" %s", &tipo);
 		printf("Nombre:%s, Apellidos:%s, DNI:%i, Tarjeta:%i, Telefono:%i, Nombre de usuario:%s, Contraseña:%s, Tipo:%c", nombre,apellidos,DNI,tarjeta,telefono,username,contrasena,tipo);
 		//Aqui se hace el registro en BD del usuario
 		seleccion = 1;
